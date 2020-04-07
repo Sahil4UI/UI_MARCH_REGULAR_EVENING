9c87f3406afb6097d9f05cc4886f371d09b6ec88
Sahil4UI <54834362+Sahil4UI@users.noreply.github.com>
2020-04-07 18:46:29 +0530

    Add files via upload

--- JS_UI/calc2.js
+++ JS_UI/calc2.js
@@ -3,14 +3,53 @@ window.addEventListener('load',function()
     numbers =document.querySelectorAll(".num");
     for(var i=0;i<numbers.length ;i++)
     {
-        numbers[i].addEventListener("click",appendOpr);
+        numbers[i].addEventListener("click",appendNum);
     }
 
-})
+    opr = document.querySelectorAll(".opr");
 
-function appendOpr()
+    for (var i=0;i<opr.length;i++)
+    {
+        opr[i].addEventListener("click",appendOpr);
+    }
+
+    document.querySelector(".clear").addEventListener("click",Clear);
+    document.querySelector(".calc").addEventListener("click",Calc);
+})
+var result;
+var isOperator= false;
+var n1 = "";
+
+function appendNum()
 {   num = event.srcElement.innerText;
    result =document.querySelector("#Result"); 
-   result.value += num;
+   n1 += num;
+   result.value = n1;
+   isOperator = false;
 }
 
+function appendOpr()
+{  
+     operator = event.srcElement.innerText;
+   result =document.querySelector("#Result"); 
+   if (!isOperator == true)
+   {
+    n1 += operator;
+    isOperator = true;
+   }
+   else{
+        n1 = n1.slice(0,n1.length-1);
+        n1 += operator;
+   }
+   
+   result.value = n1;
+}
+
+function Clear()
+{
+    result.value="";
+}
+function Calc()
+{
+   result.value = eval(result.value);
+}
