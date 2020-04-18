b867dc09d8600dbe423e388997707fd3b3fdf714
Sahil4UI <54834362+Sahil4UI@users.noreply.github.com>
2020-04-18 13:08:23 +0530

    Add files via upload

---
+++ MUSICAPP/JS/songs.js
@@ -0,0 +1,86 @@
+var songs = [
+    {
+        "song_id":1,
+        "song_name":"8 cylinder",
+        "song_url":"Assets/music/8 Cylinder - Sidhu Moose Wala (DjPunjab.Com).mp3",
+        "song_thumb":"https://miro.medium.com/max/1280/1*-8T-gFLVGwlv7wWsM0li2A.jpeg"
+    },
+    {
+        "song_id":2,
+        "song_name":"APPROACH",
+        "song_url":"Assets/music/Approach - Sidhu Moose Wala (DjPunjab.Com).mp3",
+        "song_thumb":"https://lyricsraag.com/wp-content/uploads/2020/04/APPROACH-Full-Song-Sidhu-Moose-Wala-lyrics.jpg"
+    },
+    {
+        "song_id":3,
+        "song_name":"Birthday Gift",
+        "song_url":"Assets/music/Birthday Gift.mp3",
+        "song_thumb":"https://i.ytimg.com/vi/_j1SHpXT_5E/maxresdefault.jpg"
+    },
+    {
+        "song_id":4,
+        "song_name":"Defaulter",
+        "song_url":"Assets/music/Defaulter.mp3",
+        "song_thumb":"https://i1.wp.com/wellmp3songs.com/wp-content/uploads/2019/05/Defaulter-Song-Download.jpg?resize=470%2C245&ssl=1"
+    },
+    {
+        "song_id":5,
+        "song_name":"Dismiss 141",
+        "song_url":"Assets/music/Dismiss 141 - Korala Maan (DjPunjab.Com).mp3",
+        "song_thumb":"https://www.lyricsbell.com/wp-content/uploads/2020/04/dismiss-141-korala-maan.jpg"
+    }
+,
+{
+    "song_id":6,
+    "song_name":"GendaPhool",
+    "song_url":"Assets/music/Genda Phool.mp3",
+    "song_thumb":"https://i.ytimg.com/vi/SD4Z8dlZPd8/maxresdefault.jpg"
+},
+{
+    "song_id":7,
+    "song_name":"Gud Bad",
+    "song_url":"Assets/music/Gud Bad.mp3",
+    "song_thumb":"https://www.lyricsbell.com/wp-content/uploads/2020/02/gud-bad-nawab.jpg"
+},
+{
+    "song_id":8,
+    "song_name":"Kanda Taar",
+    "song_url":"Assets/music/Kanda Taar - R Nait (DjPunjab.Com).mp3",
+    "song_thumb":"https://i.ytimg.com/vi/x2O3je76BTY/maxresdefault.jpg"
+},
+{
+    "song_id":9,
+    "song_name":"Life Style",
+    "song_url":"Assets/music/Lifestyle - Amrit Maan (DjPunjab.Com).mp3",
+    "song_thumb":"https://i.ytimg.com/vi/ahP1bzv9YlU/maxresdefault.jpg"
+},
+{
+    "song_id":10,
+    "song_name":"Expert Jatt Returns",
+    "song_url":"Assets/music/Nawabi Expert Jatt Returns.mp3",
+    "song_thumb":"https://i.ytimg.com/vi/_TQGhB1p1Ik/hqdefault.jpg"
+},
+{
+    "song_id":11,
+    "song_name":"One Side",
+    "song_url":"Assets/music/One Side - Himmat Sandhu (DjPunjab.Com).mp3",
+    "song_thumb":"https://i.ytimg.com/vi/iYo-6DOYquY/maxresdefault.jpg"
+},
+{
+    "song_id":12,
+    "song_name":"Tibbeyan da putt",
+    "song_url":"Assets/music/Sidhu Moosewala Tibbeyan Da Putt Song Download(DjBharat.IN).mp3",
+    "song_thumb":"https://i.ytimg.com/vi/twCHVhk8iMU/maxresdefault.jpg"
+}
+
+
+
+
+
+
+
+
+
+
+
+]
